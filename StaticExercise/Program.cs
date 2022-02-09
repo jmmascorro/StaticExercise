5a1c03776b111428cec231ab036cac7a6210a66b
jmmascorro <jessemascorro88@gmail.com>
2022-02-09 17:14:17 -0600

    finishedStaticExercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a temperature in Fahrenheit.");
+            double temp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your temperature in Celsius is {TempConverter.FahrenheitToCelsius(temp)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Give me a temperature in Celsius.");
+            temp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your temperature in Fahrenheit is {TempConverter.CelsiusToFahrenheit(temp)}");
+
+
+
+        }
+    }
+}
